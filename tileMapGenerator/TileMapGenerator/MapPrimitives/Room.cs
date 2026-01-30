d8f189f9c4494db60f868cdfded50b5b64128763
lingbend <lingbend@gmail.com>
2026-01-29 18:25:42 -0700

    Added GetInsidePoints to Room class

--- tileMapGenerator/TileMapGenerator/MapPrimitives/Room.cs
+++ tileMapGenerator/TileMapGenerator/MapPrimitives/Room.cs
@@ -6,6 +6,9 @@ using System.Numerics;
 using TileMapGenerator;
 using System.Collections.Concurrent;
 using CellularRoomGrower;
+using Vector2Extensions;
+using GoRogueWrapper;
+using BinaryGrid;
 
 public class Room : IDed
 {
@@ -17,12 +20,14 @@ public class Room : IDed
     public List<string> Tags{get; set;} = new List<string>();
     private int _room_id;
     public int ID { get => _room_id; set => _room_id = value; }
+    private BinaryGrid LocalGrid{get; set;}
 
     private ConcurrentDictionary<Vector2, List<Side>> growth_cache = new();
     
 
     internal Room(Vertex vertex, Func<int, Vector2, ConcurrentDictionary<Vector2, Vector2>, IEnumerable<Vector2>> shaper, IEnumerable<Vector2> valid_directions, Vector2 center, List<string>? tags = null)
     {
+        LocalGrid = new BinaryGrid(1, 1);
         Vertex = vertex;
         Locus = center;
         _room_id = UIDGenerator.GetNextID("room" + vertex.ID + center.X + center.Y);
@@ -74,6 +79,21 @@ public class Room : IDed
         var (new_sides, temp_corners) = CalculateGrowth(direction, amount);
         Corners = temp_corners;
         sides = new List<Side>(new_sides);
+
+        Vector2 max = Vector2Ext.MaxRange(Corners.Values);
+        Vector2 min = Vector2Ext.MinRange(Corners.Values);
+        Vector2 range = Vector2Ext.SpanRange(Corners.Values) + Vector2.One;
+
+        LocalGrid = new BinaryGrid((uint) range.X, (uint) range.Y);
+
+        foreach (var side in new_sides)
+        {
+            foreach (var point in side.Points)
+            {
+                LocalGrid.SetCell(point-min+Vector2.One, 1u);
+            }
+        }
+        
         growth_cache.Clear();
     }
 
@@ -112,8 +132,27 @@ public class Room : IDed
         return (grown_sides_copy, temp_corners);
     }
 
-    public IEnumerable<Vector2> GetInsides()
+    public IEnumerable<Vector2> GetInsidePoints()
     {
-        
+        ConcurrentDictionary<Vector2, bool> inside = new();
+        Vector2 min_corner = Vector2Ext.MinRange(Corners.Values);
+        Vector2 max_corner = Vector2Ext.MaxRange(Corners.Values);
+        Parallel.ForEach(Vector2Ext.Enumerate(min_corner, max_corner+Vector2.One), point =>
+        {
+            bool broken = false;
+            foreach (var neighbor in point.GetCartesianNeighbors())
+            {
+                if (inside.ContainsKey(neighbor))
+                {
+                    inside.TryAdd(point, true);
+                    break;
+                }
+            }
+            if (!broken && GoRogueWrapper.IsConnected(Locus, point+min_corner-Vector2.One, LocalGrid))
+            {
+                inside.TryAdd(point, true);
+            }
+        });
+        return inside.Keys;
     }
 }
